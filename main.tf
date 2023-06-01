364f72a67abaad997d7b4c545f06e934c4c0c007
anandctx <anandctx@outlook.com>
2023-06-01 00:36:28 +0000

    a

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "admin1" {
   name = "admin1-rg-3"
   location = "central us"
 }
+
+resource "azurerm_resource_group" "admin1" {
+  name = "admin1-rg-5"
+  location = "central us"
+}
